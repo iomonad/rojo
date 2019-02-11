976f88810245b1b52ad083b41b40f64360dc866f
Clement Trouillard <clement.TROUILLARD@canal-plus.com>
2019-02-11 16:21:20 +0100

    Cleanup

--- src/rojo/methods.clj
+++ src/rojo/methods.clj
@@ -3,15 +3,3 @@
   (:require [rojo.client :as client]
             [rojo.methods.subreddit :as sub]
             [rojo.methods.search :as search]))
-
-(let [token (client/request-token
-              {:user-client "44ttWVuhyJ7S8w"
-               :user-secret "MckC06DXMynW-Cdhfr2naP75eaU"})]
-  (->> (search/search token :query "HPPD" :limit 100)
-       (map :post)))
-
-(let [token (client/request-token
-              {:user-client "44ttWVuhyJ7S8w"
-               :user-secret "MckC06DXMynW-Cdhfr2naP75eaU"})]
-  (->> (sub/list-posts token :sub "aww" :limit 5 :sort_by :controversial)
-       (map :post)))
